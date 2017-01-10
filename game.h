b41d1ecf1b7c8fa9301c8fda7acf73d0ce21b3a8
和田　基　 <m_wada@dwango.co.jp>
2017-01-10 19:28:29 +0900

    game and player

---
+++ game.h
@@ -0,0 +1,23 @@
+#ifndef __GAME_H__
+#define __GAME_H__
+
+#include "board.h"
+#include "player.h"
+#include "list.h"
+
+typedef struct {
+    IBOARD iboard;
+    PLAYER player;
+    ELIST elist;
+} GAME, *IGAME;
+
+IGAME game_new(IBOARD iboard, PLAYER player);
+
+ELIST game_next_event_candidates(IGAME igame);
+/* returns a event_list of what can happen next.
+ * returned event_list SHOULD get freed when done using it.
+ */
+
+int game_commit_event(IGAME igame, EVENT event);
+
+#endif /* __GAME_H__ */
---
+++ player.h
@@ -0,0 +1,9 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+typedef struct {
+    int w, h;
+} PLAYER, *IPLAYER;
+
+
+#endif /* __PLAYER_H__ */
