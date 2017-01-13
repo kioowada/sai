e1a40c00c6002995b39b497b0c2c7fccca85bfc6
和田　基　 <m_wada@dwango.co.jp>
2017-01-13 20:25:48 +0900

    impl of move-on-ground

--- game.c
+++ game.c
@@ -6,6 +6,10 @@
 ////////////////////////////////////////////////////////////////////////////////////////////
 CELL _game_get_current_cell(IGAME igame);
 
+int _game_commit_event_move(IGAME igame, EP_MOVE param);
+CELL _game_get_move_target_cell(IGAME igame, EP_MOVE param);
+int _game_move_player(IGAME igame, EP_MOVE param);
+int _game_push_dice(IGAME igame, EP_MOVE param);
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // Exported Definition
@@ -22,12 +26,41 @@ IGAME game_new(IBOARD iboard, PLAYER player) {
 }
 
 int game_commit_event(IGAME igame, EVENT event) {
-    CELL now_cell;
+    EVENT_PARAM_RET eparam;
+    switch (event_type(event)) {
+        case ET_PLAYER_MOVE:
+            eparam = event_get_param(event);
+            return _game_commit_event_move(igame, eparam.param.move_param);
+    }
+    return -1;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Internal Definition
+////////////////////////////////////////////////////////////////////////////////////////////
+CELL _game_get_current_cell(IGAME igame) {
+    IBOARD iboard = igame->iboard;
+    IPLAYER iplayer = &igame->player;
+    return board_get_cell(iboard, iplayer->w, iplayer->h);
+}
+
+int _game_commit_event_move(IGAME igame, EP_MOVE param) {
+    CELL now_cell, target_cell;
 
     now_cell = _game_get_current_cell(igame);
     switch (now_cell) {
         case CELL_EMPTY:
             // TODO just move on ground, or push dice
+            target_cell = _game_get_move_target_cell(igame, param);
+            switch (target_cell) {
+                case CELL_EMPTY:
+                    _game_move_player(igame, param);
+                    break;
+                case CELL_INVALID:
+                    break;
+                default:
+                    _game_push_dice(igame, param);
+            }
             break;
         case CELL_INVALID:
             // do nothing
@@ -40,11 +73,18 @@ int game_commit_event(IGAME igame, EVENT event) {
     return 0;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////
-// Internal Definition
-////////////////////////////////////////////////////////////////////////////////////////////
-CELL _game_get_current_cell(IGAME igame) {
-    IBOARD iboard = igame->iboard;
+CELL _game_get_move_target_cell(IGAME igame, EP_MOVE param) {
     IPLAYER iplayer = &igame->player;
-    return board_get_cell(iboard, iplayer->w, iplayer->h);
+    return board_get_cell(igame->iboard, iplayer->w + param.w, iplayer->h + param.h);
+}
+
+int _game_move_player(IGAME igame, EP_MOVE param) {
+    igame->player.w += param.w;
+    igame->player.h += param.h;
+    return 0;
+}
+
+int _game_push_dice(IGAME igame, EP_MOVE param) {
+    // TODO
+    return 0;
 }
